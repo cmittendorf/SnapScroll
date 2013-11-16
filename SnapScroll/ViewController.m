ad0e0ede1b5e23811cb9dff691b3416d37728a58
Christian Mittendorf <christian.mittendorf@freenet.de>
2013-11-17 00:00:15 +0100

    fixed the small glitch when jumping back at the last item

--- SnapScroll/ViewController.m
+++ SnapScroll/ViewController.m
@@ -43,12 +43,12 @@
     _scrollView.pagingEnabled = NO;
     _scrollView.decelerationRate = UIScrollViewDecelerationRateFast;
     
-    CGFloat width = _scrollView.bounds.size.height;
-    CGFloat scrollViewWidth = width * 10 + (self.view.bounds.size.width - width);
-    _scrollView.contentSize = CGSizeMake(scrollViewWidth, width);
+    CGFloat width = _scrollView.bounds.size.width;
+    CGFloat scrollViewHeight = width * 10 + (self.view.bounds.size.height - width);
+    _scrollView.contentSize = CGSizeMake(width, scrollViewHeight);
     for (NSInteger i = 0; i < 10; i++) {
-        UIView *view = [self viewWithLabelText:[NSString stringWithFormat:@"%d", i + 1]];
-        view.frame = CGRectOffset(view.frame, i * width, 0.0f);
+        UIView *view = [self viewWithLabelText:[NSString stringWithFormat:@"%ld", i + 1]];
+        view.frame = CGRectOffset(view.frame, 0.0f, i * width);
         [_scrollView addSubview:view];
     }
 }
@@ -64,7 +64,7 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
-    _baseContentOffset = _scrollView.contentOffset.x;
+    _baseContentOffset = _scrollView.contentOffset.y;
     [_scrollView flashScrollIndicators];
 }
 
@@ -74,33 +74,27 @@
                      withVelocity:(CGPoint)velocity
               targetContentOffset:(inout CGPoint *)targetContentOffset
 {
-
-//    CGFloat width = _scrollView.bounds.size.height;
-//    NSInteger maxIndex = 10 - 1;
-//    CGFloat targetX = _scrollView.contentOffset.x + velocity.x * 30.0f;
-//    NSInteger targetIndex = lroundf(targetX / width);
-//    if (targetIndex < 0)
-//        targetIndex = 0;
-//    if (targetIndex > maxIndex)
-//        targetIndex = maxIndex;
-//    targetContentOffset->x = targetIndex * width;
-//    NSLog(@"%ld", targetIndex);
-    
-    CGFloat width = _scrollView.bounds.size.height;
-    if (velocity.x < 0) {
+    CGFloat width = _scrollView.bounds.size.width;
+    if (velocity.y < 0) {
         _baseContentOffset = MAX(0, _baseContentOffset - width);
-    } else if (velocity.x > 0) {
+    } else if (velocity.y > 0) {
         _baseContentOffset = MIN(width * 9, _baseContentOffset + width);
     }
-    targetContentOffset->x = _baseContentOffset;
 
+    // Setting the targetContentOffset->y will add a smal "jump" at the last item
+    // targetContentOffset->y = _baseContentOffset;
+
+    // Using this approach avoids the jump and smoothes the animation
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [scrollView setContentOffset:CGPointMake(0, _baseContentOffset) animated:YES];
+    });
 }
 
 #pragma mark - Internal
 
 - (UIView *)viewWithLabelText:(NSString *)text
 {
-    CGFloat width = _scrollView.bounds.size.height;
+    CGFloat width = _scrollView.bounds.size.width;
     UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, width, width)];
     view.backgroundColor = [UIColor randomColor];
     view.layer.bounds = CGRectInset(view.layer.bounds, 6, 6);
